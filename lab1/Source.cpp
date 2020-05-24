10fcdfe0e451eaffd8bb504550ee2fa605ff5e87
Avcato <64610460+Avcato@users.noreply.github.com>
2020-05-24 20:20:40 +0300

    Update Source.cpp

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -18,40 +18,18 @@ struct reader {
 /**********************************************/
 // displaying information about this reader
 void output_read(reader read) {
-	cout << "Reader ¹" << read.num << endl << endl;
+	cout << "Reader â„–" << read.num << endl << endl;
 	cout << "Surname: " << read.last_name << endl;
 	cout << "Name: " << read.name << endl;
 	cout << "Patronymic: " << read.patronymic << endl;
-	cout << "Library card ¹ " << read.number << endl;
+	cout << "Library card â„– " << read.number << endl;
 	cout << "Name of the book: " << ((read.position == 1) ? "Sherlock Holmes.Hound of baskerville" : ((read.position == 2) ? "Game of Thrones" : ((read.position == 3) ? "The Chronicles of Narnia" : "Harry Potter and the prisoner of Azkaban"))) << endl;
 	cout << "Return date: " << read.y_of_d << endl << endl;
 }
 
-void sort(reader* read, int read_num)
-{
-	for (int i = 0; i < (read_num - 1); i++)
-	{
-		if (read[i].num > read[i + 1].num)
-		{
-			int read_i_num = read[i].num;
-			read[i].num = read[i + 1].num;
-			read[i + 1].num = read_i_num;
-			reader temp = read[i];
-			read[i] = read[i + 1];
-			read[i + 1] = temp;
-		}
-	}
-}
 
-void find(reader* read, int read_num)
-{
-	int num; cout << "Enter: "; cin >> num;
-	system("cls");
-	for (int i = 0; i < read_num; i++)
-		if (num == read[i].num)
-			output_read(read[i]);
-	system("pause");
-}
+void find(reader* read, int read_num);
+
 
 int main() {
 	int a;
@@ -64,7 +42,7 @@ int main() {
 		/** menu **/
 		int input_menu;
 		cout << " 1. Entering an array of readers\n";
-		cout << " 2. Reader array outputn";
+		cout << " 2. Reader array outputn\n";
 		cout << " 3. Reader array sorting\n";
 		cout << " 4. Search in an array of readers for a given parameter\n";
 		cout << " 5. Modify a given reader\n";
@@ -84,11 +62,11 @@ int main() {
 			for (int i = 0; i < read_num; i++) {
 				system("cls");
 				read[i].num = i + 1;
-				cout << "Reader ¹" << read[i].num << endl << endl;
+				cout << "Reader â„–" << read[i].num << endl << endl;
 				cout << "Enter surname: "; cin >> read[i].last_name; cout << endl;
 				cout << "Enter name: "; cin >> read[i].name; cout << endl;
 				cout << "Enter patronomic: "; cin >> read[i].patronymic; cout << endl;
-				cout << "Library card ¹ "; cin >> read[i].number; cout << endl;
+				cout << "Library card â„– "; cin >> read[i].number; cout << endl;
 				cout << "Choose a book(1 - The Hound of the Baskervilles, 2 - Game of Thrones, 3 - The Chronicles of Narnia, 4 - Harry Potter): ";
 				int pos;
 				cin >> pos; cout << endl;
@@ -117,7 +95,7 @@ int main() {
 			// if there are no readers
 			if (!isRead || (read_num < 2)) { cout << "Error!"; Sleep(1000); continue; }
 			int type_read;
-			cout << " 1. By last name \n 2. By library card number \n 3. By selected book \nÂâîä: ";
+			cout << " 1. By last name \n 2. By library card number \n 3. By selected book \nÃ‚Ã¢Ã®Ã¤: ";
 			cin >> type_read;
 			reader read_copy;
 			for (int i = 0; i < (read_num - 1); i++)
@@ -143,7 +121,15 @@ int main() {
 				// sorting by library card number
 				else if (type_read == 2)
 				{
-					void sort();
+					if (read[i].number > read[i + 1].number)
+					{
+						int read_i_num = read[i].num;
+						read[i].num = read[i + 1].num;
+						read[i + 1].num = read_i_num;
+						read_copy = read[i];
+						read[i] = read[i + 1];
+						read[i + 1] = read_copy;
+					}
 					system("cls");
 					for (int i = 0; i < read_num; i++) {
 						output_read(read[i]);
@@ -153,7 +139,15 @@ int main() {
 				// sort by selected book
 				else if (type_read == 3)
 				{
-					void sort();
+					if (read[i].position > read[i + 1].position)
+					{
+						int read_i_num = read[i].num;
+						read[i].num = read[i + 1].num;
+						read[i + 1].num = read_i_num;
+						read_copy = read[i];
+						read[i] = read[i + 1];
+						read[i + 1] = read_copy;
+					}
 					system("cls");
 					for (int i = 0; i < read_num; i++) {
 						output_read(read[i]);
@@ -169,23 +163,63 @@ int main() {
 		{
 			if (!isRead) { cout << "Error!"; Sleep(1000); continue; } // if there aren't reader
 			int type_search; // search parameter
-			cout << " 1. Be last name\n 2. By name\n 3. By patronomic\n 4. By library card number \n 5. By name of book\n 6. By return date \nÂâîä: ";
+			cout << " 1. Be last name\n 2. By name\n 3. By patronomic\n 4. By library card number \n 5. By name of book\n 6. By return date \n: ";
 			cin >> type_search;
 			system("cls");
 			// search by last name
 			if (type_search == 1)
-			{
-				char last_name[100] = ""; cout << "Ââîä: "; cin >> last_name;
+				{
+					char last_name[100] = ""; cout << "Enter: "; cin >> last_name;
+					system("cls");
+					for (int i = 0; i < read_num; i++)
+						if (!strcmp(last_name, read[i].last_name))
+							output_read(read[i]);
+					system("pause");
+				}
+			else if (type_search == 2)
+				{
+				char name[100] = ""; cout << "Enter: "; cin >> name;
 				system("cls");
 				for (int i = 0; i < read_num; i++)
-					if (!strcmp(last_name, read[i].last_name))
+					if (!strcmp(name, read[i].name))
+						output_read(read[i]);
+				system("pause");
+				}
+			else if (type_search == 3)
+			{
+				char patronymic[100] = ""; cout << "Enter: "; cin >> patronymic;
+				system("cls");
+				for (int i = 0; i < read_num; i++)
+					if (!strcmp(patronymic, read[i].patronymic))
 						output_read(read[i]);
 				system("pause");
 			}
-
-			else if (1 < type_search < 7)
+			else if (type_search == 4)
 			{
-				void find();
+				int number; cout << "Enter: "; cin >> number;
+				system("cls");
+				for (int i = 0; i < read_num; i++)
+					if (number==read[i].number)
+						output_read(read[i]);
+				system("pause");
+			}
+			else if (type_search == 5)
+			{
+				int position; cout << "Enter: "; cin >> position;
+				system("cls");
+				for (int i = 0; i < read_num; i++)
+					if (position==read[i].position)
+						output_read(read[i]);
+				system("pause");
+			}
+			else if (type_search == 6)
+			{
+				int y_of_d; cout << "Enter: "; cin >> y_of_d;
+				system("cls");
+				for (int i = 0; i < read_num; i++)
+					if (y_of_d==read[i].y_of_d)
+						output_read(read[i]);
+				system("pause");
 			}
 
 			else { cout << "Error searching!"; continue; }
@@ -197,12 +231,12 @@ int main() {
 		{
 			if (!isRead) { cout << "Error!"; Sleep(1000); continue; } // if there aren't reader
 			unsigned short num; // numder of reader
-			cout << "Íîìåğ ÷èòàòåëÿ: "; cin >> num;
+			cout << "Number of reader: "; cin >> num;
 			unsigned short i = num - 1; // numder of reader - 1 (to work with an array)
 			// if the reader number is entered incorrectly
 			if (num > read_num || num == 0) { cout << "Error!"; continue; }
 			system("cls");
-			cout << "Reader ¹" << read[i].num << endl << endl;
+			cout << "Reader â„–" << read[i].num << endl << endl;
 			// lastname, name, patronomic
 			cout << "Enter lastname: "; cin >> read[i].last_name; cout << endl;
 			cout << "Enter name: "; cin >> read[i].name; cout << endl;
